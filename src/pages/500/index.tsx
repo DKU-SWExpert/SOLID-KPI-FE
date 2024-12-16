7b82685facec736366d0f0025054032d517cf97a
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-16 10:10:13 +0900

    feat: 500 에러 페이지를 만든다.

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,3 +1,17 @@
-const InternalError = () => {};
+import Layout from "@/components/Layout";
+import GoBackHomeButton from "@/components/Button/GoBackHomeButton";
+
+const InternalError = () => {
+  return (
+    <>
+      <Layout>
+        <h1>서버에러가 발생했습니다.</h1>
+        <small>500 Internal Server Error</small>
+        <h3>잠시 후 다시 시도해주세요</h3>
+        <GoBackHomeButton />
+      </Layout>
+    </>
+  );
+};
 
 export default InternalError;
