fc6e7b741fc8ef74a6244529072d8183b6a5b4a9
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-16 10:09:49 +0900

    feat: 404 에러 페이지를 만든다.

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,3 +1,17 @@
-const NotFound = () => {};
+import Layout from "@/components/Layout";
+import GoBackHomeButton from "@/components/Button/GoBackHomeButton";
+
+const NotFound = () => {
+  return (
+    <>
+      <Layout>
+        <h1>존재하지 않는 페이지입니다.</h1>
+        <small>404 NOT FOUND</small>
+        {/* 홈으로 돌아가기 버튼 만들기 */}
+        <GoBackHomeButton />
+      </Layout>
+    </>
+  );
+};
 
 export default NotFound;
