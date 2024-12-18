7da574f48a3c835e02f2aa28e73338e2754dcaea
digitpic <jkw5033@gmail.com>
2024-12-18 16:15:43 +0900

    refactor(internal-error): css module 적용

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,17 +1,18 @@
 import Layout from "@/components/Layout";
-import GoBackHomeButton from "@/components/Button/GoBackHomeButton";
+import GoBackHomeButton from "@components/Button/GoBackHomeButton.tsx";
+import styles from "./InternalError.module.css";
 
 const InternalError = () => {
-  return (
-    <>
-      <Layout>
-        <h1>서버에러가 발생했습니다.</h1>
-        <small>500 Internal Server Error</small>
-        <h3>잠시 후 다시 시도해주세요</h3>
-        <GoBackHomeButton />
-      </Layout>
-    </>
-  );
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>서버에러가 발생했습니다.</h1>
+                <small className={styles.errorCode}>500 Internal Server Error</small>
+                <h3 className={styles.message}>잠시 후 다시 시도해주세요</h3>
+                <GoBackHomeButton />
+            </div>
+        </Layout>
+    );
 };
 
 export default InternalError;
