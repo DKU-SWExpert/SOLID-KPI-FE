3cb1dac10b2f1bf1314d10c88e22504680a9c109
digitpic <jkw5033@gmail.com>
2024-12-18 16:14:05 +0900

    refactor(not-found): css module 적용

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,16 +1,17 @@
 import Layout from "@/components/Layout";
-import GoBackHomeButton from "@/components/Button/GoBackHomeButton";
+import GoBackHomeButton from "@components/Button/GoBackHomeButton.tsx";
+import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-  return (
-    <>
-      <Layout>
-        <h1>존재하지 않는 페이지입니다.</h1>
-        <small>404 NOT FOUND</small>
-        <GoBackHomeButton />
-      </Layout>
-    </>
-  );
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>존재하지 않는 페이지입니다.</h1>
+                <small className={styles.errorCode}>404 NOT FOUND</small>
+                <GoBackHomeButton />
+            </div>
+        </Layout>
+    );
 };
 
 export default NotFound;
