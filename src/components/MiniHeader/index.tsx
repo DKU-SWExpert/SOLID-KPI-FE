465d8051a18dae388e1d4fc4290ca6ab0ce9f7aa
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-20 00:07:20 +0900

    feat(component): 헤더 아래 존재하는 미니 헤더 구현

---
+++ src/components/MiniHeader/index.tsx
@@ -0,0 +1,14 @@
+import { CHeader } from "@coreui/react";
+
+const MiniHeader = () => {
+  return (
+    <CHeader
+      className="border-bottom"
+      style={{ height: "48px", backgroundColor: "#f8f9fa" }}
+    >
+      Home
+    </CHeader>
+  );
+};
+
+export default MiniHeader;
