5ede3fdc7d53a8d4803a477685bb3f38bfe53a0f
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-20 00:08:44 +0900

    feat(type): store 타입 변경

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,18 +1,25 @@
+import { Role } from "@/types/role";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface AuthState {
+  name: string;
+  role: Role;
   isLoggedIn: boolean;
   login: () => void;
   logout: () => void;
+  reset: () => void;
 }
 
 const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       isLoggedIn: false,
+      name: "",
+      role: "NOT_LOGGED_IN",
       login: () => set({ isLoggedIn: true }),
-      logout: () => set({ isLoggedIn: false }),
+      logout: () => set({ isLoggedIn: false, name: "", role: "NOT_LOGGED_IN" }),
+      reset: () => set({ isLoggedIn: false, name: "", role: "NOT_LOGGED_IN" }),
     }),
     {
       name: "auth-storage",
