fbdd8aa3fdf9e6bece635503a721829fe689ee28
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-20 01:50:51 +0900

    refactor(layout): 사용하지 않는 Layout 코드 제거

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,18 +1,15 @@
-import Layout from "@/components/Layout";
 import GoBackHomeButton from "@components/Button/GoBackHomeButton.tsx";
 import styles from "./InternalError.module.css";
 
 const InternalError = () => {
-    return (
-        <Layout>
-            <div className={styles.container}>
-                <h1 className={styles.heading}>서버에러가 발생했습니다.</h1>
-                <small className={styles.errorCode}>500 Internal Server Error</small>
-                <h3 className={styles.message}>잠시 후 다시 시도해주세요</h3>
-                <GoBackHomeButton />
-            </div>
-        </Layout>
-    );
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>서버에러가 발생했습니다.</h1>
+      <small className={styles.errorCode}>500 Internal Server Error</small>
+      <h3 className={styles.message}>잠시 후 다시 시도해주세요</h3>
+      <GoBackHomeButton />
+    </div>
+  );
 };
 
 export default InternalError;
