979173001c2dd0a878fba95971e8a0b7de546bf8
Jhzlo <kjh0010703@naver.com>
2024-12-21 01:04:04 +0900

    feat(sw-expert): title component 추가

--- src/pages/student/scholarship/swExpert/index.tsx
+++ src/pages/student/scholarship/swExpert/index.tsx
@@ -1,5 +1,45 @@
+import { CCard, CCardHeader } from "@coreui/react";
+
 const SwExpert = () => {
-    return <div>SW Expert 페이지</div>;
-}
+    return (
+        <div className="container px-4">
+            <div className="d-flex align-items-center mt-4 mb-4">
+                <CCard
+                    textBgColor="primary"
+                    className="w-100 d-flex align-items-center justify-content-center"
+                    style={{maxWidth: "15rem", height: "4.5rem"}}
+                >
+                    <CCardHeader className="text-white text-center fw-semibold fs-5">
+                        SW Expert 신청
+                    </CCardHeader>
+                </CCard>
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+        </div>
+        );
+};
 
 export default SwExpert;
