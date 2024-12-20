fbfdc96a6e95879f2cbff6a7a6125f1a5b123686
digitpic <jkw5033@gmail.com>
2024-12-21 00:28:26 +0900

    refactor(common-card): common card 분리

---
+++ src/components/CommonCard/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CCard, CCardHeader, CCardBody } from '@coreui/react';
+
+interface CommonCardProps {
+    header: string;
+    children: React.ReactNode;
+    textBgColor: string;
+    style?: React.CSSProperties;
+}
+
+const CommonCard: React.FC<CommonCardProps> = ({ header, children, textBgColor, style }) => {
+    return (
+        <CCard textBgColor={textBgColor} className="mt-4 mb-4 ms-2 me-2" style={style}>
+            <CCardHeader className="text-white">{header}</CCardHeader>
+            <CCardBody className="bg-dawn">{children}</CCardBody>
+        </CCard>
+    );
+};
+
+export default CommonCard;
