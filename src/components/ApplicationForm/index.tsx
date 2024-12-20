873af4c5790b9bec6b5a51e3c0b249e688f3171f
digitpic <jkw5033@gmail.com>
2024-12-21 00:31:41 +0900

    refactor(application-form): application form 분리

---
+++ src/components/ApplicationForm/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { CRow, CCol, CFormInput, CFormLabel } from "@coreui/react";
+import CommonCard from "@components/CommonCard";
+
+const ApplicationForm: React.FC = () => {
+    return (
+        <CommonCard header="신청서 (기본정보)" textBgColor="info" style={{ maxWidth: "100rem" }}>
+            <CRow>
+                <CCol xs="12" md="6" lg="4">
+                    <CFormLabel>학번</CFormLabel>
+                    <CFormInput
+                        type="text"
+                        className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                        placeholder="12345678"
+                        readOnly
+                    />
+
+                    <CFormLabel>생년월일</CFormLabel>
+                    <CFormInput type="date" className="mb-4 bg-dawn text-white border-gray" />
+
+                    <CFormLabel>이메일</CFormLabel>
+                    <CFormInput type="text" className="bg-dawn text-white border-gray" />
+                </CCol>
+
+                <CCol xs="12" md="6" lg="4">
+                    <CFormLabel>이름</CFormLabel>
+                    <CFormInput
+                        type="text"
+                        className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                        placeholder="홍길동"
+                        readOnly
+                    />
+
+                    <CFormLabel>학년</CFormLabel>
+                    <CFormInput type="text" className="mb-4 bg-dawn text-white border-gray gray-placeholder" />
+                </CCol>
+
+                <CCol xs="12" md="12" lg="4">
+                    <CFormLabel>학과</CFormLabel>
+                    <CFormInput
+                        type="text"
+                        className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                        placeholder="소프트웨어"
+                        readOnly
+                    />
+
+                    <CFormLabel>휴대폰</CFormLabel>
+                    <CFormInput type="text" className="mb-4 bg-dawn text-white border-gray gray-placeholder" />
+
+                    <CFormLabel>TOPCIT ID</CFormLabel>
+                    <CFormInput type="text" className="mb-4 bg-dawn text-white border-gray gray-placeholder" />
+                </CCol>
+            </CRow>
+        </CommonCard>
+    );
+};
+
+export default ApplicationForm;
