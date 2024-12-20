54711a1e938896691a6d1d1c28b6e09ef1afd50d
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-20 18:33:34 +0900

    feat(constant): 하드코딩 되어있던 Role을 상수로 분리

--- src/types/role.ts
+++ src/types/role.ts
@@ -1,6 +1,14 @@
+import {
+  STUDENT,
+  PROFESSOR,
+  ADMIN,
+  SUPER_ADMIN,
+  NOT_A_MEMBER,
+} from "@/constants/role";
+
 export type Role =
-  | "STUDENT"
-  | "PROFESSOR"
-  | "ADMIN"
-  | "SUPER_ADMIN"
-  | "NOT_A_MEMBER";
+  | typeof STUDENT
+  | typeof PROFESSOR
+  | typeof ADMIN
+  | typeof SUPER_ADMIN
+  | typeof NOT_A_MEMBER;
