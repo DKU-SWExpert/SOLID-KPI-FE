20c174e47bbfe2fab2902f7beaa7f9f73d01f359
digitpic <jkw5033@gmail.com>
2024-12-20 20:32:16 +0900

    feat(app): sw test request page router 추가

---
+++ src/pages/SwTestRequest/index.tsx
@@ -0,0 +1,11 @@
+import {CCard} from "@coreui/react";
+
+const SwTestRequest = () => {
+    return (
+        <CCard textBgColor="info">
+
+        </CCard>
+    )
+}
+
+export default SwTestRequest;
