018926d9c0d5c68b316a3f6cfc0c7ca2437e3c77
digitpic <jkw5033@gmail.com>
2024-12-21 00:32:36 +0900

    refactor(save-button): 저장 버튼 분리

---
+++ src/components/SaveButton/index.tsx
@@ -0,0 +1,11 @@
+import { CButton, CCard } from "@coreui/react";
+
+const SaveButton = () => {
+    return (
+        <CCard className="mt-5 mb-4 ms-2 me-2">
+            <CButton color="primary">저 장</CButton>
+        </CCard>
+    );
+};
+
+export default SaveButton;
