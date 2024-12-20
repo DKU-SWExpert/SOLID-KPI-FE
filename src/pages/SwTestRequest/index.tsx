a1be9eaaebf3e1859d0bea57224017d572dcb69e
digitpic <jkw5033@gmail.com>
2024-12-20 23:57:11 +0900

    feat(sw-test-request): 신청서 card 추가

--- src/pages/SwTestRequest/index.tsx
+++ src/pages/SwTestRequest/index.tsx
@@ -1,4 +1,4 @@
-import {CButton, CCard, CCardBody, CCardHeader, CFormInput, CFormSelect} from "@coreui/react";
+import {CButton, CCard, CCardBody, CCardHeader, CCol, CFormInput, CFormLabel, CFormSelect, CRow} from "@coreui/react";
 
 const SwTestRequest = () => {
     return (
@@ -33,7 +33,91 @@ const SwTestRequest = () => {
             <CCard textBgColor="info" className="mt-4 mb-4 ms-2 me-2" style={{maxWidth: "100rem"}}>
                 <CCardHeader className="text-white">신청서 (기본정보)</CCardHeader>
                 <CCardBody className="bg-dawn">
+                    <CRow>
+                        <CCol xs="12" md="6" lg="4">
+                            <div className="text-white">
+                                <CFormLabel>
+                                    학번
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                                    placeholder="12345678"
+                                    readOnly
+                                />
+                            </div>
 
+                            <div className="text-white">
+                                <CFormLabel>
+                                    생년월일
+                                </CFormLabel>
+                                <CFormInput
+                                    type="date"
+                                    className="mb-4 bg-dawn text-white border-gray"
+                                />
+                            </div>
+
+                            <div className="text-white">
+                                <CFormLabel>
+                                    이메일
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="bg-dawn text-white border-gray"
+                                />
+                            </div>
+                        </CCol>
+                        <CCol xs="12" md="6" lg="4">
+                            <div className="text-white">
+                                <CFormLabel>
+                                    이름
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                                    placeholder="홍길동"
+                                    readOnly
+                                />
+
+                                <CFormLabel>
+                                    학년
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                                />
+                            </div>
+                        </CCol>
+                        <CCol xs="12" md="12" lg="4">
+                            <div className="text-white">
+                                <CFormLabel>
+                                    학과
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                                    placeholder="소프트웨어"
+                                    readOnly
+                                />
+
+                                <CFormLabel>
+                                    휴대폰
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                                />
+
+                                <CFormLabel>
+                                    TOPCIT ID
+                                </CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    className="mb-4 bg-dawn text-white border-gray gray-placeholder"
+                                />
+                            </div>
+                        </CCol>
+                    </CRow>
                 </CCardBody>
             </CCard>
 
