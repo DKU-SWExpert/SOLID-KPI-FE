92572f1f11d9d260a59c8b2ece253c07a4167ab4
digitpic <jkw5033@gmail.com>
2024-12-20 22:42:39 +0900

    feat(sw-test-request): title card 추가

--- src/pages/SwTestRequest/index.tsx
+++ src/pages/SwTestRequest/index.tsx
@@ -1,10 +1,12 @@
-import {CCard} from "@coreui/react";
+import {CCard, CCardHeader} from "@coreui/react";
 
 const SwTestRequest = () => {
     return (
-        <CCard textBgColor="info">
-
-        </CCard>
+        <>
+            <CCard textBgColor="primary" className="mt-4 mb-4 ms-2" style={{maxWidth: "15rem"}}>
+                <CCardHeader className="text-white">SW 역량 테스트 신청</CCardHeader>
+            </CCard>
+        </>
     )
 }
 
