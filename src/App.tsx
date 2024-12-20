83b53cef06e00c098c9ae547d8c0191456cbbe18
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-20 18:52:49 +0900

    chore(initial state): 불필요한 주석 삭제 및 개발을 위해 초기 role을 STUDENT로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PublicRoute from "@routes/PublicRoute";
 import NotFound from "@pages/404";
 import Layout from "@components/Layout";
 import Login from "@pages/Login";
+import Home from "@pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/">
-          {/* 
-          role 별로 페이지를 구현해야함.
-          Home을 만들고 그 안에 구현할지, 
-          role 별로 페이지를 만들어서 라우팅할지 아직 미정
-        */}
-        </Route>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Route>
       <Route
