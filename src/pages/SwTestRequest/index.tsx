6f586518627f5ded8b4bc94217c032823fdf50f5
digitpic <jkw5033@gmail.com>
2024-12-20 23:37:13 +0900

    feat(sw-test-request): 저장 버튼 card 추가

--- src/pages/SwTestRequest/index.tsx
+++ src/pages/SwTestRequest/index.tsx
@@ -1,4 +1,4 @@
-import {CCard, CCardBody, CCardHeader, CFormInput, CFormSelect} from "@coreui/react";
+import {CButton, CCard, CCardBody, CCardHeader, CFormInput, CFormSelect} from "@coreui/react";
 
 const SwTestRequest = () => {
     return (
@@ -7,7 +7,7 @@ const SwTestRequest = () => {
                 <CCardHeader className="text-white">SW 역량 테스트 신청</CCardHeader>
             </CCard>
 
-            <CCard textBgColor="info" className="mt-4 mb-4 ms-2" style={{maxWidth: "100rem"}}>
+            <CCard textBgColor="info" className="mt-4 mb-4 ms-2 me-2" style={{maxWidth: "100rem"}}>
                 <CCardHeader className="text-white">시험 구분</CCardHeader>
                 <CCardBody className="bg-dawn">
                     <CFormSelect className="bg-dawn text-white border-gray" style={{maxWidth: "15rem"}}>
@@ -19,7 +19,7 @@ const SwTestRequest = () => {
                 </CCardBody>
             </CCard>
 
-            <CCard textBgColor="info" className="mt-4 mb-4 ms-2" style={{maxWidth: "100rem"}}>
+            <CCard textBgColor="info" className="mt-4 mb-4 ms-2 me-2" style={{maxWidth: "100rem"}}>
                 <CCardHeader className="text-white">시험 일자</CCardHeader>
                 <CCardBody className="bg-dawn">
                     <CFormInput
@@ -28,6 +28,17 @@ const SwTestRequest = () => {
                         style={{maxWidth: "15rem"}}
                     />
                 </CCardBody>
+            </CCard>
+
+            <CCard textBgColor="info" className="mt-4 mb-4 ms-2 me-2" style={{maxWidth: "100rem"}}>
+                <CCardHeader className="text-white">신청서 (기본정보)</CCardHeader>
+                <CCardBody className="bg-dawn">
+
+                </CCardBody>
+            </CCard>
+
+            <CCard className="mt-5 mb-4 ms-2 me-2">
+                <CButton color="primary">저 장</CButton>
             </CCard>
         </>
     )
