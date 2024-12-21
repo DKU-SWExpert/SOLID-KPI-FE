3ebe6b2ebe9c7b8e9bbb9e44c51e3300903a474e
digitpic <jkw5033@gmail.com>
2024-12-21 13:44:23 +0900

    refactor(test-category): 시험 종류 map 으로 관리

--- src/components/TestCategory/index.tsx
+++ src/components/TestCategory/index.tsx
@@ -2,13 +2,21 @@ import { CFormSelect } from "@coreui/react";
 import CommonCard from "@components/CommonCard";
 
 const TestCategory = () => {
+    const tests = {
+        topcit: "TOPCIT",
+        pccp: "PCCP",
+        pce: "PCCE",
+    };
+
     return (
         <CommonCard header="시험 구분" textBgColor="info" style={{ maxWidth: "100rem" }}>
             <CFormSelect className="bg-dawn text-white border-gray" style={{ maxWidth: "15rem" }}>
                 <option>시험 선택</option>
-                <option value="topcit">TOPCIT</option>
-                <option value="pccp">PCCP</option>
-                <option value="pcce">PCCE</option>
+                {Object.entries(tests).map(([key, value]) => (
+                    <option key={key} value={key}>
+                        {value}
+                    </option>
+                ))}
             </CFormSelect>
         </CommonCard>
     );
