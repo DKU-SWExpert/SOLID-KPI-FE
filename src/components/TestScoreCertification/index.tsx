9f6a3d3b68483ef9593e845ef9d8f091bb79da39
digitpic <jkw5033@gmail.com>
2024-12-22 18:19:31 +0900

    feat(test-score-certification): 컴포넌트 생성

---
+++ src/components/TestScoreCertification/index.tsx
@@ -0,0 +1,24 @@
+import {CFormInput} from "@coreui/react";
+import CommonCard from "@components/CommonCard";
+import React from "react";
+
+interface TestScoreProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+const TestScore: React.FC<TestScoreProps> = ({value, onChange}) => {
+    return (
+        <CommonCard header="성적 증명서" textBgColor="info" style={{maxWidth: "100rem"}}>
+            <CFormInput
+                type="file"
+                className="mb-4 bg-dawn text-white border-gray"
+                style={{maxWidth: "15rem"}}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </CommonCard>
+    );
+};
+
+export default TestScore;
