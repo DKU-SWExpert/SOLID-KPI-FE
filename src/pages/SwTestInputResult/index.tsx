f86be7e29b9ab4952b41782f99e564ef53c3daa1
digitpic <jkw5033@gmail.com>
2024-12-22 18:00:09 +0900

    feat(test-level): 컴포넌트 생성

--- src/pages/SwTestInputResult/index.tsx
+++ src/pages/SwTestInputResult/index.tsx
@@ -3,6 +3,7 @@ import CommonCard from "@components/CommonCard";
 import TestCategory from "@components/TestCategory";
 import TestDate from "@components/TestDate";
 import SaveButton from "@components/SaveButton";
+import TestLevel from "@components/TestLevel";
 
 const SwTestRequest = () => {
     const [formData, setFormData] = useState({
@@ -38,7 +39,7 @@ const SwTestRequest = () => {
             return;
         }
         if (!formData.file) {
-            alert("성적 증명서를 입력해 주세요.");
+            alert("성적 증명서를 첨부해 주세요.");
             return;
         }
     };
@@ -54,6 +55,10 @@ const SwTestRequest = () => {
             <TestDate
                 value={formData.date}
                 onChange={(value) => handleUpdate("date", value)}
+            />
+            <TestLevel
+                value={formData.level}
+                onChange={(value) => handleUpdate("level", value)}
             />
             <SaveButton onClick={handleSave}/>
         </>
