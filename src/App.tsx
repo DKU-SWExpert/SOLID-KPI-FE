bcc9d60c0e04b3422188d4294cc84d78a4b9e5c4
digitpic <jkw5033@gmail.com>
2024-12-22 17:53:38 +0900

    feat(sw-test-input-result): 컴포넌트 재사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,54 @@
-import { Routes, Route } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import PublicRoute from "@routes/PublicRoute";
 import NotFound from "@pages/404";
 import Layout from "@components/Layout";
 import Login from "@pages/Login";
-import SwTestRequest from "@pages/SwTestRequest";
+import SwTestInputResult from "@pages/SwTestInputResult";
 
 function App() {
-  // const { role } = useAuthStore();
+    // const { role } = useAuthStore();
 
-  // if (role === "NOT_LOGGED_IN") {
-  //   return <Login />;
-  // }
+    // if (role === "NOT_LOGGED_IN") {
+    //   return <Login />;
+    // }
 
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/">
-          {/* 
+    return (
+        <Routes>
+            <Route element={<Layout/>}>
+                <Route path="/">
+                    {/*
           role 별로 페이지를 구현해야함.
           Home을 만들고 그 안에 구현할지, 
           role 별로 페이지를 만들어서 라우팅할지 아직 미정
         */}
-        </Route>
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-                <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <PublicRoute>
-              <NotFound />
-            </PublicRoute>
-          }
-        />
-          <Route
-            path="/sw-test/request"
-            element={
-              <PublicRoute>
-                <SwTestRequest />
-              </PublicRoute>
-            }
-          />
-      </Route>
-    </Routes>
-  );
+                </Route>
+                <Route
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <Login/>
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    path="*"
+                    element={
+                        <PublicRoute>
+                            <NotFound/>
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    path="/sw-test/request"
+                    element={
+                        <PublicRoute>
+                            <SwTestInputResult/>
+                        </PublicRoute>
+                    }
+                />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
