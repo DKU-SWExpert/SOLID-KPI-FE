4beed4a44218a63d0743c39f258b6ecca34affa3
HoyeongJeon <hoyeong0302@gmail.com>
2024-12-23 01:07:18 +0900

    fix(conflict): 라우팅 컨플릭트 해결

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,19 @@ import PublicRoute from "@routes/PublicRoute";
 import NotFound from "@pages/404";
 import Layout from "@components/Layout";
 import Login from "@pages/Login";
-import SwTestInputResult from "@pages/SwTestInputResult";
 import Home from "@pages/Home";
 import StudentRoutes from "@routes/StudentRoutes";
 import ProtectedRoute from "@routes/ProtectedRoute";
+
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/">
-          {/* 
-          role 별로 페이지를 구현해야함.
-          Home을 만들고 그 안에 구현할지, 
-          role 별로 페이지를 만들어서 라우팅할지 아직 미정
-        */}
-        </Route>
         <Route
           path="/login"
           element={
             <PublicRoute>
               <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <PublicRoute>
-              <NotFound />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/sw-test/request"
-          element={
-            <PublicRoute>
-              <SwTestInputResult />
             </PublicRoute>
           }
         />
--- src/routes/StudentRoutes.tsx
+++ src/routes/StudentRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Application from "@/pages/Student/Internship/Application";
 import EnterResult from "@/pages/Student/Internship/EnterResult";
+import SwTestInputResult from "@pages/SwTestInputResult";
 
 const StudentRoutes = () => {
   return (
     <Routes>
       <Route path="internship/application" element={<Application />} />
       <Route path="internship/enter-result" element={<EnterResult />} />
+      <Route path="sw-test/request" element={<SwTestInputResult />} />
     </Routes>
   );
 };
