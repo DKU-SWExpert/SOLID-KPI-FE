f0a05a5c342616e52f8779a4f2346826695ef581
digitpic <jkw5033@gmail.com>
2024-12-25 14:59:47 +0900

    feat(internal-error): 500 페이지 배경 색상 변경

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,14 +1,15 @@
 import GoBackHomeButton from "@components/Button/GoBackHomeButton.tsx";
 import styles from "./InternalError.module.css";
+import "@styles/custom-color.css";
 
 const InternalError = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>서버에러가 발생했습니다.</h1>
-      <small className={styles.errorCode}>500 Internal Server Error</small>
-      <h3 className={styles.message}>잠시 후 다시 시도해주세요</h3>
-      <GoBackHomeButton />
-    </div>
+      <div className={`${styles.container} bg-dawn-light`}>
+          <h1 className={`${styles.heading} text-white`}>서버에러가 발생했습니다.</h1>
+          <small className={styles.errorCode}>500 Internal Server Error</small>
+          <h3 className={`${styles.message} text-white`}>잠시 후 다시 시도해주세요</h3>
+          <GoBackHomeButton/>
+      </div>
   );
 };
 
