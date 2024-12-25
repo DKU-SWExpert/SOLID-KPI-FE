5459270f7e8a03dde7b2adcb8c8b642ed7be5af8
digitpic <jkw5033@gmail.com>
2024-12-25 14:53:21 +0900

    feat(not-found): 404 페이지 배경 색상 변경

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -3,11 +3,11 @@ import styles from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>존재하지 않는 페이지입니다.</h1>
-      <small className={styles.errorCode}>404 NOT FOUND</small>
-      <GoBackHomeButton />
-    </div>
+      <div className={`${styles.container} bg-dawn-light`}>
+          <h1 className={`${styles.heading} text-white`}>존재하지 않는 페이지입니다.</h1>
+          <small className={styles.errorCode}>404 NOT FOUND</small>
+          <GoBackHomeButton/>
+      </div>
   );
 };
 
