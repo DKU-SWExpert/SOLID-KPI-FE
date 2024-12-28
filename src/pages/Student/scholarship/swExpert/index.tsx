70e6257c2d351c07ca56f91bc0eb69868b5c75a7
Jhzlo <kjh0010703@naver.com>
2024-12-28 23:52:50 +0900

    refactor(sw-expert): UI 크기 조정

--- src/pages/Student/scholarship/swExpert/index.tsx
+++ src/pages/Student/scholarship/swExpert/index.tsx
@@ -135,7 +135,7 @@ const SwExpert = () => {
                 >
                     <CFormSelect
                         className="bg-dawn-light text-white border-gray col-md-4"
-                        style={{maxWidth: "15rem"}}
+                        style={{maxWidth: "25rem"}}
                         value={formData.selectedPeriod}
                         onChange={(e) =>
                             handleFormChange("selectedPeriod", e.target.value)
