ca82f7ffb762527b63af646136f32049bf606a39
digitpic <jkw5033@gmail.com>
2024-12-31 20:33:49 +0900

    feat(professor-routes): internship route 추가

--- src/routes/ProfessorRoutes.tsx
+++ src/routes/ProfessorRoutes.tsx
@@ -1,4 +1,5 @@
 import SolidCloud from "@/pages/Professor/SolidCloud";
+<<<<<<< HEAD
 import SwTest from "@/pages/Professor/SwTest"
 import {Route, Routes} from "react-router-dom";
 
@@ -9,6 +10,18 @@ const ProfessorRoutes = () => {
             <Route path="sw-test" element={<SwTest/>}/>
         </Routes>
     );
+=======
+import { Route, Routes } from "react-router-dom";
+import ProfessorInternship from "@pages/Professor/Internship";
+
+const ProfessorRoutes = () => {
+  return (
+    <Routes>
+      <Route path="solid-cloud" element={<SolidCloud />} />
+      <Route path="internship" element={<ProfessorInternship />} />
+    </Routes>
+  );
+>>>>>>> f5eea8c (feat(professor-routes): internship route 추가)
 };
 
 export default ProfessorRoutes;
