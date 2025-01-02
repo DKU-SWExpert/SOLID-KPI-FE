64214050b55a8621a2df81c7dcd2d7a4ce28fb6b
Jhzlo <kjh0010703@naver.com>
2025-01-02 12:21:21 +0900

    refactor(sw-test): 반복된 구조 컴포넌트화

--- src/pages/Professor/SwTest/index.tsx
+++ src/pages/Professor/SwTest/index.tsx
@@ -46,114 +46,65 @@ const createBaseChartOptions = (stepSize: number, max: number) => ({
     },
 });
 
+const ChartCard = ({ years, title, chartData, chartOptions }) => (
+    <CCol className="mt-4">
+        <CCard className="bg-dark text-white border-gray" style={{ height: "550px" }}>
+            <CCardHeader className="bg-dawn-light test-white">
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <select className="form-select bg-dark text-white" style={{ width: "100px", marginRight: "10px" }}>
+                            {years.map((year) => (
+                                <option key={year} value={year}>
+                                    {year}
+                                </option>
+                            ))}
+                        </select>
+                        <span style={{ fontSize: "1.2rem", color: "#fff" }}>{title}</span>
+                    </div>
+                    <CButton color="primary">다운로드</CButton>
+                </div>
+            </CCardHeader>
+            <CCardBody className="bg-dawn">
+                <div style={{ height: "450px" }}>
+                    <Bar data={chartData} options={chartOptions} />
+                </div>
+            </CCardBody>
+        </CCard>
+    </CCol>
+);
+
+
 const SwTest = () => {
     const years = ["2024", "2025", "2026", "2027", "2028", "2029"];
     const { chartData, levelChartData, departmentChartData, gradeChartData } = useChartStore();
 
-    const chartOptions = createBaseChartOptions(50, 100);
-    const levelChartOptions = createBaseChartOptions(5, 15);
-    const departmentChartOptions = createBaseChartOptions(1, 6);
-    const gradeChartOptions = createBaseChartOptions(1, 6);
-
     return (
         <CContainer fluid className="body flex-grow-1 px-4">
             <CContainer className="my-5">
-                <CCol>
-                    <CCard className="bg-dawn-light text-white border-gray" style={{ height: "550px" }}>
-                        <CCardHeader className="bg-dawn-light test-white">
-                            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <div style={{ display: "flex", alignItems: "center" }}>
-                                    <select className="form-select bg-dark text-white" style={{ width: "100px", marginRight: "10px" }}>
-                                        {years.map((year) => (
-                                            <option key={year} value={year}>
-                                                {year}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <span style={{ fontSize: "1.2rem", color: "#fff" }}>취득인원</span>
-                                </div>
-                                <CButton color="primary">다운로드</CButton>
-                            </div>
-                        </CCardHeader>
-                        <CCardBody className="bg-dawn">
-                            <div style={{ height: "450px" }}>
-                                <Bar data={chartData} options={chartOptions} />
-                            </div>
-                        </CCardBody>
-                    </CCard>
-                </CCol>
-                <CCol className="mt-4">
-                    <CCard className="bg-dark text-white border-gray" style={{ height: "550px" }}>
-                        <CCardHeader className="bg-dawn-light test-white">
-                            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <div style={{ display: "flex", alignItems: "center" }}>
-                                    <select className="form-select bg-dark text-white" style={{ width: "100px", marginRight: "10px" }}>
-                                        {years.map((year) => (
-                                            <option key={year} value={year}>
-                                                {year}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <span style={{ fontSize: "1.2rem", color: "#fff" }}>TOPCIT 취득 Level</span>
-                                </div>
-                                <CButton color="primary">다운로드</CButton>
-                            </div>
-                        </CCardHeader>
-                        <CCardBody className="bg-dawn">
-                            <div style={{ height: "450px" }}>
-                                <Bar data={levelChartData} options={levelChartOptions} />
-                            </div>
-                        </CCardBody>
-                    </CCard>
-                </CCol>
-                <CCol className="mt-4">
-                    <CCard className="bg-dark text-white border-gray" style={{ height: "550px" }}>
-                        <CCardHeader className="bg-dawn-light test-white">
-                            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <div style={{ display: "flex", alignItems: "center" }}>
-                                    <select className="form-select bg-dark text-white" style={{ width: "100px", marginRight: "10px" }}>
-                                        {years.map((year) => (
-                                            <option key={year} value={year}>
-                                                {year}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <span style={{ fontSize: "1.2rem", color: "#fff" }}>학과별 TOPCIT 취득 인원</span>
-                                </div>
-                                <CButton color="primary">다운로드</CButton>
-                            </div>
-                        </CCardHeader>
-                        <CCardBody className="bg-dawn">
-                            <div style={{ height: "450px" }}>
-                                <Bar data={departmentChartData} options={departmentChartOptions} />
-                            </div>
-                        </CCardBody>
-                    </CCard>
-                </CCol>
-                <CCol className="mt-4">
-                    <CCard className="bg-dark text-white border-gray" style={{ height: "550px" }}>
-                        <CCardHeader className="bg-dawn-light test-white">
-                            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <div style={{ display: "flex", alignItems: "center" }}>
-                                    <select className="form-select bg-dark text-white" style={{ width: "100px", marginRight: "10px" }}>
-                                        {years.map((year) => (
-                                            <option key={year} value={year}>
-                                                {year}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <span style={{ fontSize: "1.2rem", color: "#fff" }}>학년별 TOPCIT 취득 Level 취득인력</span>
-                                </div>
-                                <CButton color="primary">다운로드</CButton>
-                            </div>
-                        </CCardHeader>
-                        <CCardBody className="bg-dawn">
-                            <div style={{ height: "450px" }}>
-                                <Bar data={gradeChartData} options={gradeChartOptions} />
-                            </div>
-                        </CCardBody>
-                    </CCard>
-                </CCol>
+                <ChartCard
+                    years={years}
+                    title="취득인원"
+                    chartData={chartData}
+                    chartOptions={createBaseChartOptions(50, 100)}
+                />
+                <ChartCard
+                    years={years}
+                    title="TOPCIT 취득 Level"
+                    chartData={levelChartData}
+                    chartOptions={createBaseChartOptions(5, 15)}
+                />
+                <ChartCard
+                    years={years}
+                    title="학과별 TOPCIT 취득 인원"
+                    chartData={departmentChartData}
+                    chartOptions={createBaseChartOptions(1, 6)}
+                />
+                <ChartCard
+                    years={years}
+                    title="학년별 TOPCIT 취득 Level 취득인력"
+                    chartData={gradeChartData}
+                    chartOptions={createBaseChartOptions(1, 6)}
+                />
             </CContainer>
         </CContainer>
     );
