a6532e2d8cfceff5193d8827e165ac72336a9561
digitpic <jkw5033@gmail.com>
2025-01-03 01:04:44 +0900

    feat(professor-internship): 연도별 차트 데이터 변경 기능 추가

--- src/pages/Professor/Internship/index.tsx
+++ src/pages/Professor/Internship/index.tsx
@@ -1,5 +1,5 @@
-import React, {useRef} from "react";
-import {CButton, CCard, CCardBody, CCardHeader, CContainer} from "@coreui/react";
+import React, {useRef, useState} from "react";
+import {CButton, CCard, CCardBody, CCardHeader, CContainer, CFormSelect} from "@coreui/react";
 import {
     ArcElement,
     BarController,
@@ -9,9 +9,10 @@ import {
     Legend,
     LinearScale,
     LineController,
-    LineElement, PieController,
+    LineElement,
+    PieController,
     PointElement,
-    Tooltip,
+    Tooltip
 } from 'chart.js';
 import {Bar, Chart, Pie} from "react-chartjs-2";
 import {useChartStore} from "@store/internship";
@@ -19,17 +20,8 @@ import CIcon from "@coreui/icons-react";
 import {cilArrowThickToBottom} from "@coreui/icons";
 
 ChartJS.register(
-    LinearScale,  // 선형 스케일 (y 축 값)
-    CategoryScale, // 카테고리 스케일 (x 축 값)
-    BarElement, // 바 차트 요소
-    PointElement, // 점 요소 (선 차트에 필요)
-    LineElement, // 라인 차트 요소
-    Legend, // 범례
-    Tooltip, // 툴팁
-    LineController, // 라인 차트 컨트롤러
-    BarController, // 바 차트 컨트롤러
-    PieController, // 파이 차트 컨트롤러
-    ArcElement, // 파이 차트 요소
+    LinearScale, CategoryScale, BarElement, PointElement, LineElement, Legend, Tooltip,
+    LineController, BarController, PieController, ArcElement
 );
 
 const downloadChart = (chartRef: React.RefObject<ChartJS<'bar' | 'line' | 'pie'>>, filename: string) => {
@@ -43,25 +35,24 @@ const downloadChart = (chartRef: React.RefObject<ChartJS<'bar' | 'line' | 'pie'>
 };
 
 const ProfessorInternship = () => {
+    const [departmentInternshipStatusYear, setDepartmentInternshipStatusYear] = useState('2024');
+    const [internshipStatusByTypeYear, setInternshipStatusByTypeYear] = useState('2024');
+
     const mixChartRef = useRef<ChartJS<'bar' | 'line'> | null>(null);
     const barChartRef = useRef<ChartJS<'bar'> | null>(null);
     const pieChartRef = useRef<ChartJS<'pie'> | null>(null);
 
-    const {
-        internshipCompletion,
-        departmentInternshipStatus,
-        internshipStatusByType
-    } = useChartStore();
+    const {internshipCompletion, departmentInternshipStatus, internshipStatusByType} = useChartStore();
 
-    const chartStyle = {
-        height: '100%',
-        width: 'auto',
+    const chartStyle = {height: '100%', width: 'auto'};
+    const cardBodyStyle = {display: 'flex', justifyContent: 'center', alignItems: 'center'};
+
+    const handleDepartmentInternshipStatusYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setDepartmentInternshipStatusYear(event.target.value);
     };
 
-    const cardBodyStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+    const handleInternshipStatusByTypeYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setInternshipStatusByTypeYear(event.target.value);
     };
 
     return (
@@ -85,14 +76,29 @@ const ProfessorInternship = () => {
             <CContainer className="d-flex justify-content-center my-5">
                 <CCard className="bg-dark text-white border-gray" style={{width: '100%', maxWidth: '1200px'}}>
                     <CCardHeader className="d-flex justify-content-between align-items-center px-4 py-3">
-                        <span>학과별 인턴 현황</span>
+                        <CContainer className="d-flex align-items-center">
+                            <CFormSelect
+                                className="bg-dawn-light text-white border-gray"
+                                value={departmentInternshipStatusYear}
+                                onChange={handleDepartmentInternshipStatusYearChange}
+                                style={{marginRight: '10px', width: '150px'}}
+                            >
+                                <option value="2024">2024</option>
+                                <option value="2025">2025</option>
+                                <option value="2026">2026</option>
+                                <option value="2027">2027</option>
+                                <option value="2028">2028</option>
+                                <option value="2029">2029</option>
+                            </CFormSelect>
+                            <span>학과별 인턴 현황</span>
+                        </CContainer>
                         <CButton color="primary" className="bg-transparent border-0"
                                  onClick={() => downloadChart(barChartRef, 'department-internship-status.png')}>
                             <CIcon icon={cilArrowThickToBottom} size="lg"/>
                         </CButton>
                     </CCardHeader>
                     <CCardBody style={{...cardBodyStyle, height: '500px', overflow: 'hidden'}}>
-                        <Bar data={departmentInternshipStatus} ref={barChartRef}
+                        <Bar data={departmentInternshipStatus[departmentInternshipStatusYear]} ref={barChartRef}
                              style={chartStyle}/>
                     </CCardBody>
                 </CCard>
@@ -101,7 +107,22 @@ const ProfessorInternship = () => {
             <CContainer className="d-flex justify-content-center my-5">
                 <CCard className="bg-dark text-white border-gray" style={{width: '100%', maxWidth: '1200px'}}>
                     <CCardHeader className="d-flex justify-content-between align-items-center px-4 py-3">
-                        <span>인턴 유형별 현황</span>
+                        <CContainer className="d-flex align-items-center">
+                            <CFormSelect
+                                className="bg-dawn-light text-white border-gray"
+                                value={internshipStatusByTypeYear}
+                                onChange={handleInternshipStatusByTypeYearChange}
+                                style={{marginRight: '10px', width: '150px'}}
+                            >
+                                <option value="2024">2024</option>
+                                <option value="2025">2025</option>
+                                <option value="2026">2026</option>
+                                <option value="2027">2027</option>
+                                <option value="2028">2028</option>
+                                <option value="2029">2029</option>
+                            </CFormSelect>
+                            <span>인턴 유형별 현황</span>
+                        </CContainer>
                         <CButton color="primary" className="bg-transparent border-0"
                                  onClick={() => downloadChart(pieChartRef, 'internship-status-by-type.png')}>
                             <CIcon icon={cilArrowThickToBottom} size="lg"/>
@@ -109,7 +130,7 @@ const ProfessorInternship = () => {
                     </CCardHeader>
                     <CCardBody className="pt-0 d-flex justify-content-center align-items-center"
                                style={{height: '500px'}}>
-                        <Pie data={internshipStatusByType} ref={pieChartRef}
+                        <Pie data={internshipStatusByType[internshipStatusByTypeYear]} ref={pieChartRef}
                              style={{height: '400px', width: 'auto'}}/>
                     </CCardBody>
                 </CCard>
