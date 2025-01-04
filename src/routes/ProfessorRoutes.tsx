f8c9aa6cbf14f45eff20230693838da8937fd9a0
dndqls <dndqls1125@dankook.ac.kr>
2025-01-05 01:50:51 +0900

    feat(professor-award): 페이지 생성

--- src/routes/ProfessorRoutes.tsx
+++ src/routes/ProfessorRoutes.tsx
@@ -8,23 +8,17 @@ import CompetitionContest from "@/pages/Professor/CompetitionContest";
 import { Route, Routes } from "react-router-dom";
 
 const ProfessorRoutes = () => {
-  return (
-    <Routes>
-      <Route path="solid-cloud" element={<SolidCloud />} />
-      <Route path="sw-test" element={<SwTest />} />
-      <Route path="internship" element={<ProfessorInternship />} />
-      <Route
-        path="industry-academia-project"
-        element={<IndustryAcademiaProject />}
-      />
-      <Route path="award" element={<Award />} />
-      <Route path="number-of-student" element={<NumOfStudent />} />
-      <Route path="award" element={<Award />} />
-      <Route path="number-of-student" element={<NumOfStudent />} />
-      <Route path="internship" element={<ProfessorInternship />} />
-      <Route path="competition-contest" element={<CompetitionContest />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="solid-cloud" element={<SolidCloud/>}/>
+            <Route path="sw-test" element={<SwTest/>}/>
+            <Route path="internship" element={<ProfessorInternship />}/>
+            <Route path="industry-academia-project" element={<IndustryAcademiaProject />}/>
+            <Route path="award" element={<Award/>}/>
+            <Route path="number-of-student" element={<NumOfStudent/>}/>
+            <Route path="competition-contest" element={<CompetitionContest />} />
+        </Routes>
+    );
 };
 
 export default ProfessorRoutes;
