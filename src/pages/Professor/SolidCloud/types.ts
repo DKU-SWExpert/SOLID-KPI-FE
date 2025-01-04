54bacec73e2f7a8b8c455486be03b3c2c77434d2
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-04 18:46:23 +0900

    feat(types): 타입 분리

---
+++ src/pages/Professor/SolidCloud/types.ts
@@ -0,0 +1,45 @@
+export type ChartData = {
+  labels: string[];
+  datasets: {
+    label: string;
+    backgroundColor: string | string[];
+    borderColor: string | string[];
+    borderWidth: number;
+    data: number[];
+  }[];
+} | null;
+
+export interface ChartDataType {
+  labels: string[];
+  datasets: {
+    type: "bar" | "line";
+    label: string;
+    data: number[];
+    backgroundColor?: string;
+    borderColor?: string | string[];
+    borderWidth?: number;
+    tension?: number;
+    order?: number;
+  }[];
+}
+
+export interface DataTableProps {
+  data: ChartDataType;
+}
+
+export interface ChartCardProps {
+  years: number[];
+  title: string;
+  initialData: {
+    labels: string[];
+    datasets: {
+      label: string;
+      backgroundColor: string | string[];
+      borderColor: string | string[];
+      borderWidth: number;
+      data: number[];
+    }[];
+  } | null;
+  chartOptions: any;
+  onYearChange?: (year: number) => void;
+}
