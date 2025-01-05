23c7f858ab74e71c67e555366e1e9cd52ab64efb
jun562 <capboy290@naver.com>
2025-01-06 05:30:36 +0900

    feat(page): 학과별/년도별 산학 프로젝트 참여율 차트 추가

--- src/pages/Professor/IndustryAcademiaProject/index.tsx
+++ src/pages/Professor/IndustryAcademiaProject/index.tsx
@@ -27,19 +27,28 @@ ChartJS.register(
 
 const IndustryAcademiaProject = () => {
 
-  const barChartRef = useRef<ChartJS<'bar'> | null>(null);
+  const years = [2024, 2025, 2026, 2027, 2028, 2029];
 
-  const {participationRate} = useChartStore();
+  const participation_barChartRef = useRef<ChartJS<'bar'> | null>(null);
+  const deptYear_participation_barChartRef = useRef<ChartJS<'bar'> | null>(null);
+
+  const {participationRate, deptYear_participationRate} = useChartStore();
+
+  const [deptYear_participationRateYear, setDeptYear_participationRateYear] = useState("2024");
+
+  const handleDeptYear_participationRateYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDeptYear_participationRateYear(event.target.value);
+  }
 
   return (
     <CContainer fluid className="flex-grow-1 px-4 body mt-5 mb-5">
       <ChartCard
         className="mb-5"
         title="참여율 현황"
-        chartRef={barChartRef}
+        chartRef={participation_barChartRef}
         chartData={participationRate}
         chartType={Bar}
-        fileName="industry_academia_project_participation_rate.png"
+        fileName="industry-academia-project-participation-rate.png"
         chartOptions={{
           scales: {
             y: {
@@ -58,10 +67,37 @@ const IndustryAcademiaProject = () => {
           },
         }}
         showYearSelect={false}
-        />
-    </CContainer>
-  )
+      />
 
+      <ChartCard
+        className="mb-5"
+        title='학과별/년도별 산학 프로젝트 참여율'
+        year={deptYear_participationRateYear}
+        onYearChange= {handleDeptYear_participationRateYearChange}
+        years={years}
+        chartRef={deptYear_participation_barChartRef}
+        chartData={deptYear_participationRate[deptYear_participationRateYear]}
+        chartType={Bar}
+        fileName="dept&year-industry-academia-project-participation-rate.png"
+        chartOptions={{
+          scales: {
+            y: {
+              max: 16, // y축 최대값을 16로 설정
+            },
+          },
+          plugins: {
+              datalabels: {
+                  anchor: 'end',
+                  align: 'top',
+                  formatter: (value) => `${value} %`,
+                  color: 'white',
+                  font: {size: 12},
+              },
+          },
+        }}
+      />
+    </CContainer>
+  );
 };
 
 export default IndustryAcademiaProject;
--- src/store/industryAcademiaProject.ts
+++ src/store/industryAcademiaProject.ts
@@ -4,6 +4,7 @@ import {ChartData} from "chart.js";
 
 interface ChartState {
   participationRate: ChartData<'bar' | 'line'>;
+  deptYear_participationRate: Record<string, ChartData<'bar'>>;
   reset: () => void;
 }
 
@@ -26,15 +27,87 @@ const participationRate: ChartData<'bar' | 'line'> = {
 ],
 }
 
+const deptYear_participationRate: Record<string, ChartData<'bar'>> = {
+  '2024': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets:[
+      {
+        label: '2024',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        data: [7, 9, 10, 11, 12, 11],
+        type: 'bar',
+      }
+    ]
+  },
+  '2025': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets:[
+      {
+        label: '2024',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        data: [10, 9, 9, 7, 10, 11],
+        type: 'bar',
+      }
+    ]
+  },
+  '2026': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets:[
+      {
+        label: '2024',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        data: [7, 11, 10, 11, 12, 11],
+        type: 'bar',
+      }
+    ]
+  },
+  '2027': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets:[
+      {
+        label: '2024',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        data: [7, 9, 13, 11, 12, 11],
+        type: 'bar',
+      }
+    ]
+  },
+  '2028': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets:[
+      {
+        label: '2024',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        data: [7, 9, 10, 11, 15, 11],
+        type: 'bar',
+      }
+    ]
+  },
+  '2029': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets:[
+      {
+        label: '2024',
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        data: [7, 9, 10, 11, 10, 11],
+        type: 'bar',
+      }
+    ]
+  }
+}
+
+
 const useChartStore = create<ChartState>()(
   persist(
       (set) => ({
           participationRate,
+          deptYear_participationRate,
           reset: () => set({
             participationRate,
+            deptYear_participationRate,
           }),
       }),
-      {name: "chart-storage"}
+      {name: "chartStorage"}
   )
 );
 
