e5340b4b5e1ca349b2b4dbfb4ab867be9614bf8b
jun562 <capboy290@naver.com>
2025-01-05 20:39:49 +0900

    feat(page): 학과별 경진대회 참여자 차트 추가

--- src/store/competitionContest.ts
+++ src/store/competitionContest.ts
@@ -4,7 +4,7 @@ import {persist} from "zustand/middleware";
 
 interface ChartState {
   participant: ChartData<'bar'>;
-  
+  departmentParticipant: Record<string, ChartData<'bar'>>;
   reset: () => void;
 }
 
@@ -25,12 +25,67 @@ const participant : ChartData<'bar'> = {
   ],
 };
 
+const departmentParticipant: Record<string, ChartData<'bar'>> = {
+  '2024': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets: [
+      {label: '단체', data: [9, 3, 9, 0, 8, 8], backgroundColor: 'rgba(54, 162, 235, 0.6)'},
+      {label: '개인', data: [1, 1, 1, 1, 3, 1], backgroundColor: 'rgba(255, 159, 64, 0.6)'},
+      {label: '종합', data: [10, 4, 10, 1, 11, 9], backgroundColor: 'rgba(75, 192, 192, 0.6)'}
+    ]
+  },
+  '2025': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets: [
+      {label: '단체', data: [1, 1, 1, 1, 1, 1], backgroundColor: 'rgba(54, 162, 235, 0.6)'},
+      {label: '개인', data: [1, 1, 1, 1, 1, 1], backgroundColor: 'rgba(255, 159, 64, 0.6)'},
+      {label: '종합', data: [2, 2, 2, 2, 2, 2], backgroundColor: 'rgba(75, 192, 192, 0.6)'}
+    ]
+  },
+  '2026': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets: [
+      {label: '단체', data: [1, 1, 1, 1, 1, 1], backgroundColor: 'rgba(54, 162, 235, 0.6)'},
+      {label: '개인', data: [1, 1, 1, 1, 1, 2], backgroundColor: 'rgba(255, 159, 64, 0.6)'},
+      {label: '종합', data: [2, 2, 2, 2, 2, 3], backgroundColor: 'rgba(75, 192, 192, 0.6)'}
+    ]
+  },
+  '2027': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets: [
+      {label: '단체', data: [1, 1, 1, 1, 1, 1], backgroundColor: 'rgba(54, 162, 235, 0.6)'},
+      {label: '개인', data: [1, 1, 1, 1, 1, 3], backgroundColor: 'rgba(255, 159, 64, 0.6)'},
+      {label: '종합', data: [2, 2, 2, 2, 2, 4], backgroundColor: 'rgba(75, 192, 192, 0.6)'}
+    ]
+  },
+  '2028': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets: [
+      {label: '단체', data: [1, 1, 1, 1, 1, 1], backgroundColor: 'rgba(54, 162, 235, 0.6)'},
+      {label: '개인', data: [1, 1, 1, 1, 1, 4], backgroundColor: 'rgba(255, 159, 64, 0.6)'},
+      {label: '종합', data: [2, 2, 2, 2, 2, 5], backgroundColor: 'rgba(75, 192, 192, 0.6)'}
+    ]
+  },
+  '2029': {
+    labels: ['SW융합학부', '데이터사이언스', '모바일', '사이버보안', '소프트웨어', '컴퓨터공학'],
+    datasets: [
+      {label: '단체', data: [1, 1, 1, 1, 1, 1], backgroundColor: 'rgba(54, 162, 235, 0.6)'},
+      {label: '개인', data: [1, 1, 1, 1, 1, 5], backgroundColor: 'rgba(255, 159, 64, 0.6)'},
+      {label: '종합', data: [2, 2, 2, 2, 2, 6], backgroundColor: 'rgba(75, 192, 192, 0.6)'}
+    ]
+  },
+
+
+}
+
 const useChartStore = create<ChartState>()(
   persist(
     (set) => ({
       participant,
+      departmentParticipant,
       reset: () => set({
         participant,
+        departmentParticipant,
       }),
     }),
     {name: "chartStorage"}
