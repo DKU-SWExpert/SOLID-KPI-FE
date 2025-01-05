dbcde0558e7e43e63e566d44a207e950540537df
jun562 <capboy290@naver.com>
2025-01-06 04:23:04 +0900

    feat(route, navigation): 라우팅 및 네비게이션 경로 추가

--- src/routes/ProfessorRoutes.tsx
+++ src/routes/ProfessorRoutes.tsx
@@ -1,6 +1,7 @@
 import SolidCloud from "@/pages/Professor/SolidCloud";
 import SwTest from "@/pages/Professor/SwTest"
 import ProfessorInternship from "@pages/Professor/Internship";
+import IndustryAcademiaProject from "@pages/Professor/IndustryAcademiaProject";
 import {Route, Routes} from "react-router-dom";
 
 const ProfessorRoutes = () => {
@@ -8,7 +9,8 @@ const ProfessorRoutes = () => {
         <Routes>
             <Route path="solid-cloud" element={<SolidCloud/>}/>
             <Route path="sw-test" element={<SwTest/>}/>
-            <Route path="internship" element={<ProfessorInternship />} />
+            <Route path="internship" element={<ProfessorInternship />}/>
+            <Route path="industry-academia-project" element={<IndustryAcademiaProject />}/>
         </Routes>
     );
 };
