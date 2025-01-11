d244523a3450b940cee9cf07830b7e5dd7be934c
digitpic <jkw5033@gmail.com>
2025-01-11 21:49:51 +0900

    feat(super-admin-routes): Add internship page path

--- src/routes/SuperAdminRoutes.tsx
+++ src/routes/SuperAdminRoutes.tsx
@@ -1,9 +1,11 @@
 import {Route, Routes} from "react-router-dom";
 import FileStore from "@pages/Professor/FileStore";
+import SuperAdminInternship from "@pages/SuperAdmin/Internship";
 
 const SuperAdminRoutes = () => {
     return (
         <Routes>
+            <Route path="internship" element={<SuperAdminInternship/>}/>
             <Route path="file-store-management" element={<FileStore/>}/>
         </Routes>
     );
