99e297098c19f1928b9f465f223552398f16ac53
Jhzlo <kjh0010703@naver.com>
2025-01-11 22:36:11 +0900

    feat(route): super admin route 경로 설정

--- src/routes/SuperAdminRoutes.tsx
+++ src/routes/SuperAdminRoutes.tsx
@@ -1,9 +1,23 @@
 import {Route, Routes} from "react-router-dom";
 import FileStore from "@pages/Professor/FileStore";
+import SolidCloud from "@pages/Professor/SolidCloud";
+import SwTest from "@pages/Professor/SwTest";
+import ProfessorInternship from "@pages/Professor/Internship";
+import IndustryAcademiaProject from "@pages/Professor/IndustryAcademiaProject";
+import Award from "@pages/Professor/Award";
+import NumOfStudent from "@pages/Professor/NumOfStudent";
+import CompetitionContest from "@pages/Professor/CompetitionContest";
 
 const SuperAdminRoutes = () => {
     return (
         <Routes>
+            <Route path="solid-cloud" element={<SolidCloud/>}/>
+            <Route path="sw-test" element={<SwTest/>}/>
+            <Route path="internship" element={<ProfessorInternship />}/>
+            <Route path="industry-academia-project" element={<IndustryAcademiaProject />}/>
+            <Route path="award" element={<Award/>}/>
+            <Route path="number-of-student" element={<NumOfStudent/>}/>
+            <Route path="competition-contest" element={<CompetitionContest />} />
             <Route path="file-store-management" element={<FileStore/>}/>
         </Routes>
     );
