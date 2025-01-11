6a62505c72e26ca736d1e6121a0e5f5a6c51ab6e
jun562 <capboy290@naver.com>
2025-01-11 15:47:00 +0900

    feat(table): 2024년 임시 테이블 리스트 렌더링

--- src/pages/Professor/CompetitionContest/index.tsx
+++ src/pages/Professor/CompetitionContest/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {useRef, useState} from "react";
-import {CContainer} from "@coreui/react";
-import {Bar, Pie} from "react-chartjs-2";
-import {useChartStore} from "@store/competitionContest";
+import { useRef, useState } from "react";
+import { CContainer } from "@coreui/react";
+import { Bar, Pie } from "react-chartjs-2";
+import { useChartStore } from "@store/competitionContest";
 import ChartCard from "@/components/ChartCard";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import {
@@ -11,7 +11,8 @@ import {
   CTableHeaderCell,
   CTableRow,
   CTableBody,
-  CTableDataCell
+  CTableDataCell,
+  CCard,
 } from "@coreui/react";
 import {
   ArcElement,
@@ -25,32 +26,51 @@ import {
   LineElement,
   PieController,
   PointElement,
-  Tooltip
+  Tooltip,
 } from "chart.js";
 
 ChartJS.register(
-  LinearScale, CategoryScale, BarElement, PointElement, LineElement, Legend, Tooltip,
-  LineController, BarController, PieController, ArcElement, ChartDataLabels
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  PieController,
+  ArcElement,
+  ChartDataLabels
 );
 
 const CompetitionContest = () => {
-  const years = [2024,2025,2026,2027,2028,2029];
+  const years = [2024, 2025, 2026, 2027, 2028, 2029];
 
-  const participantBarChartRef = useRef<ChartJS<'bar'> | null>(null);
-  const deptParticipantBarChartRef = useRef<ChartJS<'bar'> | null>(null);
-  const pieChartRef = useRef<ChartJS<'pie'> | null>(null);
+  const year = "2024";
 
-  const {participant, departmentParticipant, manpowerByCompetition} = useChartStore();
+  const participantBarChartRef = useRef<ChartJS<"bar"> | null>(null);
+  const deptParticipantBarChartRef = useRef<ChartJS<"bar"> | null>(null);
+  const pieChartRef = useRef<ChartJS<"pie"> | null>(null);
 
-  const [departmentParticipantYear, setDepartmentParticipantYear] = useState("2024");
-  const [manpowerByCompetitionYear, setManpowerByCompetitionYear] = useState("2024");
+  const { participant, departmentParticipant, manpowerByCompetition } =
+    useChartStore();
 
-  const handleDepartmentParticipantYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const [departmentParticipantYear, setDepartmentParticipantYear] =
+    useState("2024");
+  const [manpowerByCompetitionYear, setManpowerByCompetitionYear] =
+    useState("2024");
+
+  const handleDepartmentParticipantYearChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setDepartmentParticipantYear(event.target.value);
-  }
-  const handleManpowerByCompetitionYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  };
+  const handleManpowerByCompetitionYearChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setManpowerByCompetitionYear(event.target.value);
-  }
+  };
 
   return (
     <CContainer fluid className="flex-grow-1 px-4 body mt-5 mb-5">
@@ -64,21 +84,21 @@ const CompetitionContest = () => {
         chartOptions={{
           plugins: {
             datalabels: {
-              anchor: 'center',
-              align: 'center',
-              formatter: (value) => value !== 0 ? `${value} 명` : '',
-              color: 'white',
-              font: {size: 12},
+              anchor: "center",
+              align: "center",
+              formatter: (value) => (value !== 0 ? `${value} 명` : ""),
+              color: "white",
+              font: { size: 12 },
             },
           },
         }}
         showYearSelect={false}
-        />
+      />
 
       <ChartCard
-        title='학과별 경진대회 참여자'
+        title="학과별 경진대회 참여자"
         year={departmentParticipantYear}
-        onYearChange= {handleDepartmentParticipantYearChange}
+        onYearChange={handleDepartmentParticipantYearChange}
         years={years}
         chartRef={deptParticipantBarChartRef}
         chartData={departmentParticipant[departmentParticipantYear]}
@@ -91,81 +111,82 @@ const CompetitionContest = () => {
             },
           },
           plugins: {
-              datalabels: {
-                  anchor: 'end',
-                  align: 'top',
-                  formatter: (value) => `${value} 명`,
-                  color: 'white',
-                  font: {size: 12},
-              },
+            datalabels: {
+              anchor: "end",
+              align: "top",
+              formatter: (value) => `${value} 명`,
+              color: "white",
+              font: { size: 12 },
+            },
           },
-      }}
+        }}
       />
-
-      <CTable className="mb-5 text-white border-gray text-center">
-        <CTableHead>
-          <CTableRow>
-            <CTableHeaderCell scope="col" ></CTableHeaderCell>
-            <CTableHeaderCell scope="col">SW융합학부</CTableHeaderCell>
-            <CTableHeaderCell scope="col">데이터사이언스</CTableHeaderCell>
-            <CTableHeaderCell scope="col">모바일</CTableHeaderCell>
-            <CTableHeaderCell scope="col">사이버보안</CTableHeaderCell>
-            <CTableHeaderCell scope="col">소프트웨어</CTableHeaderCell>
-            <CTableHeaderCell scope="col">컴퓨터공학</CTableHeaderCell>
-          </CTableRow>
-        </CTableHead>
-        <CTableBody>
-          <CTableRow>
-            <CTableHeaderCell scope="row">단체</CTableHeaderCell>
-            <CTableDataCell>9</CTableDataCell>
-            <CTableDataCell>3</CTableDataCell>
-            <CTableDataCell>9</CTableDataCell>
-            <CTableDataCell>0</CTableDataCell>
-            <CTableDataCell>8</CTableDataCell>
-            <CTableDataCell>8</CTableDataCell>
-          </CTableRow>
-          <CTableRow>
-            <CTableHeaderCell scope="row">개인</CTableHeaderCell>
-            <CTableDataCell>1</CTableDataCell>
-            <CTableDataCell>1</CTableDataCell>
-            <CTableDataCell>1</CTableDataCell>
-            <CTableDataCell>1</CTableDataCell>
-            <CTableDataCell>3</CTableDataCell>
-            <CTableDataCell>1</CTableDataCell>
-          </CTableRow>
-          <CTableRow>
-            <CTableHeaderCell scope="row">종합</CTableHeaderCell>
-            <CTableDataCell>10</CTableDataCell>
-            <CTableDataCell>4</CTableDataCell>
-            <CTableDataCell>10</CTableDataCell>
-            <CTableDataCell>1</CTableDataCell>
-            <CTableDataCell>11</CTableDataCell>
-            <CTableDataCell>9</CTableDataCell>
-          </CTableRow>
-        </CTableBody>
-      </CTable>
+      <CCard
+        className="bg-dark border-gray mb-5 px-4 py-2"
+        style={{ borderRadius: "0.25rem" }}
+      >
+        <CTable className="table-dark text-white text-center border-gray">
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell></CTableHeaderCell>
+              {departmentParticipant["2024"].labels &&
+                departmentParticipant["2024"].labels.map((value, index) => (
+                  <CTableHeaderCell key={index}>
+                    {String(value)}
+                  </CTableHeaderCell>
+                ))}
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow>
+              <CTableHeaderCell>단체</CTableHeaderCell>
+              {departmentParticipant["2024"].datasets[0].data.map(
+                (value, index) => (
+                  <CTableDataCell key={index}>{value}</CTableDataCell>
+                )
+              )}
+            </CTableRow>
+            <CTableRow>
+              <CTableHeaderCell>개인</CTableHeaderCell>
+              {departmentParticipant["2024"].datasets[1].data.map(
+                (value, index) => (
+                  <CTableDataCell key={index}>{value}</CTableDataCell>
+                )
+              )}
+            </CTableRow>
+            <CTableRow>
+              <CTableHeaderCell>종합</CTableHeaderCell>
+              {departmentParticipant["2024"].datasets[2].data.map(
+                (value, index) => (
+                  <CTableDataCell key={index}>{value}</CTableDataCell>
+                )
+              )}
+            </CTableRow>
+          </CTableBody>
+        </CTable>
+      </CCard>
 
       <ChartCard
-                title="대회별 인력"
-                year={manpowerByCompetitionYear}
-                onYearChange={handleManpowerByCompetitionYearChange}
-                years={years}
-                chartRef={pieChartRef}
-                chartData={manpowerByCompetition[manpowerByCompetitionYear]}
-                chartType={Pie}
-                fileName="manpower-by-competition.png"
-                chartOptions={{
-                    plugins: {
-                        datalabels: {
-                            anchor: 'center',
-                            align: 'center',
-                            formatter: (value) => value !== 0 ? `${value} 명` : '',
-                            color: 'white',
-                            font: {size: 12},
-                        },
-                    },
-                }}
-            />
+        title="대회별 인력"
+        year={manpowerByCompetitionYear}
+        onYearChange={handleManpowerByCompetitionYearChange}
+        years={years}
+        chartRef={pieChartRef}
+        chartData={manpowerByCompetition[manpowerByCompetitionYear]}
+        chartType={Pie}
+        fileName="manpower-by-competition.png"
+        chartOptions={{
+          plugins: {
+            datalabels: {
+              anchor: "center",
+              align: "center",
+              formatter: (value) => (value !== 0 ? `${value} 명` : ""),
+              color: "white",
+              font: { size: 12 },
+            },
+          },
+        }}
+      />
     </CContainer>
   );
 };
