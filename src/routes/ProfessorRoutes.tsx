a89d077ff8e3fc9d60a203fc90354ab884e6753a
dndqls <dndqls1125@dankook.ac.kr>
2025-01-11 15:22:24 +0900

    fix:충돌제거

--- src/routes/ProfessorRoutes.tsx
+++ src/routes/ProfessorRoutes.tsx
@@ -2,6 +2,8 @@ import SolidCloud from "@/pages/Professor/SolidCloud";
 import SwTest from "@/pages/Professor/SwTest"
 import ProfessorInternship from "@pages/Professor/Internship";
 import IndustryAcademiaProject from "@pages/Professor/IndustryAcademiaProject";
+import NumOfStudent from "@/pages/Professor/NumOfStudent"
+import Award from "@/pages/Professor/Award"
 import {Route, Routes} from "react-router-dom";
 
 const ProfessorRoutes = () => {
@@ -11,6 +13,8 @@ const ProfessorRoutes = () => {
             <Route path="sw-test" element={<SwTest/>}/>
             <Route path="internship" element={<ProfessorInternship />}/>
             <Route path="industry-academia-project" element={<IndustryAcademiaProject />}/>
+            <Route path="award" element={<Award/>}/>
+            <Route path="number-of-student" element={<NumOfStudent/>}/>
         </Routes>
     );
 };
