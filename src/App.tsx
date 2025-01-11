e85ad9645197cfe69cd6c21955344f4059afa29b
Jhzlo <kjh0010703@naver.com>
2025-01-11 23:23:45 +0900

    feat(app): admin router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import PublicRoute from "@routes/PublicRoute";
 import NotFound from "@pages/404";
 import Layout from "@components/Layout";
@@ -8,48 +8,57 @@ import StudentRoutes from "@routes/StudentRoutes";
 import ProtectedRoute from "@routes/ProtectedRoute";
 import ProfessorRoutes from "@routes/ProfessorRoutes";
 import SuperAdminRoutes from "@routes/SuperAdminRoutes.tsx";
+import AdminRoutes from "@routes/AdminRoutes.tsx"
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/student/*"
-          element={
-            <ProtectedRoute>
-              <StudentRoutes />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/professor/*"
-          element={
-            <ProtectedRoute>
-              <ProfessorRoutes />
-            </ProtectedRoute>
-          }
-        />
-         <Route
-           path="/super-admin/*"
-           element={
-             <ProtectedRoute>
-               <SuperAdminRoutes />
-             </ProtectedRoute>
-           }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route element={<Layout/>}>
+                <Route
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <Login/>
+                        </PublicRoute>
+                    }
+                />
+                <Route path="/" element={<Home/>}/>
+                <Route
+                    path="/student/*"
+                    element={
+                        <ProtectedRoute>
+                            <StudentRoutes/>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/professor/*"
+                    element={
+                        <ProtectedRoute>
+                            <ProfessorRoutes/>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/admin/*"
+                    element={
+                        <ProtectedRoute>
+                            <AdminRoutes/>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/super-admin/*"
+                    element={
+                        <ProtectedRoute>
+                            <SuperAdminRoutes/>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="*" element={<NotFound/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
