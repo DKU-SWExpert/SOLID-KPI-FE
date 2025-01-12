da2ebdb2f141c9562d4e36c3b6c027120470ab77
jun562 <capboy290@naver.com>
2025-01-13 02:49:56 +0900

    feat(routing): 수퍼관리자, 관리자 라우팅 설정

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import FileStore from "@/pages/Management/FileStore";
 import SolidCloud from "@pages/Dashboard/SolidCloud";
 import SwTest from "@/pages/Dashboard/SwTest";
@@ -8,21 +8,26 @@ import Award from "@/pages/Dashboard/Award";
 import NumOfStudent from "@/pages/Dashboard/NumOfStudent";
 import CompetitionContest from "@/pages/Dashboard/CompetitionContest";
 import ProfessorManagement from "@pages/Management/Professor";
+import ItemManagement from "@/pages/Management/Item";
 
 const AdminRoutes = () => {
-    return (
-        <Routes>
-            <Route path="solid-cloud" element={<SolidCloud/>}/>
-            <Route path="sw-test" element={<SwTest/>}/>
-            <Route path="internship" element={<Internship />}/>
-            <Route path="industry-academia-project" element={<IndustryAcademiaProject />}/>
-            <Route path="award" element={<Award/>}/>
-            <Route path="number-of-student" element={<NumOfStudent/>}/>
-            <Route path="competition-contest" element={<CompetitionContest />} />
-            <Route path="file-store-management" element={<FileStore/>}/>
-            <Route path="professor-management" element={<ProfessorManagement/>} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="solid-cloud" element={<SolidCloud />} />
+      <Route path="sw-test" element={<SwTest />} />
+      <Route path="internship" element={<Internship />} />
+      <Route
+        path="industry-academia-project"
+        element={<IndustryAcademiaProject />}
+      />
+      <Route path="award" element={<Award />} />
+      <Route path="number-of-student" element={<NumOfStudent />} />
+      <Route path="competition-contest" element={<CompetitionContest />} />
+      <Route path="file-store-management" element={<FileStore />} />
+      <Route path="professor-management" element={<ProfessorManagement />} />
+      <Route path="item-management" element={<ItemManagement />} />
+    </Routes>
+  );
 };
 
 export default AdminRoutes;
