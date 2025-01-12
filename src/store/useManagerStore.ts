a364f58306b3b6d5db0d7e49646560d5e9be69c2
ghyeon <ggh01140@gmail.com>
2025-01-13 01:21:43 +0900

    데이터 추가, 및 페이지 이동시 중복 데이터 생기는 문제 해결

--- src/store/useManagerStore.ts
+++ src/store/useManagerStore.ts
@@ -27,9 +27,28 @@ interface ManagerStore {
 export const useManagerStore = create<ManagerStore>((set) => ({
   // 초기 데이터
   managers: [
-    { id: 25, name: '홍길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
-    { id: 24, name: '홍길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
-    // ... 필요에 따라 추가
+    { id: 1, name: '홍길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 2, name: '김길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 3, name: '박길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 4, name: '이길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 5, name: '권길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 6, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 7, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 8, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 9, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 10, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 11, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 12, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 13, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 14, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 15, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 16, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 17, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 18, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    { id: 19, name: '성길동', phone: '010-1234-1234', email: 'manager@dku.ac.kr', department: '학교 본부' },
+    
+
+    // 필요에 따라 추가
   ],
 
   search: '',
