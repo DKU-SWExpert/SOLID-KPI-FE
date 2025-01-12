f60b4374a5f8b371455a20d63e4b9835530a8a44
jun562 <capboy290@naver.com>
2025-01-13 04:12:03 +0900

    feat(page): 아이템관리 페이지 구현

--- src/pages/Management/Item/index.tsx
+++ src/pages/Management/Item/index.tsx
@@ -1,4 +1,41 @@
+import { useState } from "react";
+import { useItemStore } from "@store/item";
+import { CContainer, CCol, CRow } from "@coreui/react";
+import ItemTable from "@components/ItemTable";
+import SearchBar from "@components/SearchBar";
+import Pagination from "@components/Pagination";
+import AddButton from "@pages/Management/Item/AddButton";
+
 const ItemManagement = () => {
-  return <div>hi</div>;
+  const { data, search } = useItemStore();
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const pageCount = Math.ceil(filteredData.length / itemsPerPage);
+
+  const handlePageClick = (event: { selected: number }) => {
+    setCurrentPage(event.selected);
+  };
+
+  return (
+    <CContainer className="py-4">
+      <CRow className="mb-3">
+        <CCol sm={4}>
+          <SearchBar />
+        </CCol>
+        <CCol sm={8} className="text-end">
+          <AddButton />
+        </CCol>
+      </CRow>
+
+      <ItemTable currentPage={currentPage} itemsPerPage={itemsPerPage} />
+
+      <Pagination pageCount={pageCount} onPageChange={handlePageClick} />
+    </CContainer>
+  );
 };
 export default ItemManagement;
