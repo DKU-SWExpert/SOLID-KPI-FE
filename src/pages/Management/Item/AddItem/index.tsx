01597fe876073714728b3e26981647b2f30f5d66
jun562 <capboy290@naver.com>
2025-01-27 02:31:06 +0900

    feat(addPage): 아이템 추가 페이지 구현

--- src/pages/Management/Item/AddItem/index.tsx
+++ src/pages/Management/Item/AddItem/index.tsx
@@ -1,4 +1,45 @@
+import { useState } from "react";
+import {
+  CButton,
+  CCol,
+  CContainer,
+  CFormInput,
+  CFormLabel,
+} from "@coreui/react";
+
 const AddItem = () => {
-  return <div>hi</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+  });
+
+  const handleChange = (field: string, value: string) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
+
+  const handleSave = () => {
+    alert("저장되었습니다!");
+  };
+
+  return (
+    <CContainer className="py-4">
+      <CCol className="mb-5" md={6}>
+        <CFormLabel className="text-white">이름</CFormLabel>
+        <CFormInput
+          className="text-white border-gray bg-dark"
+          value={formData.name}
+          onChange={(e) => handleChange("name", e.target.value)}
+        />
+      </CCol>
+      <CButton
+        className="mt-4 text-center"
+        color="primary"
+        onClick={handleSave}
+        style={{ width: "100%" }}
+      >
+        저 장
+      </CButton>
+    </CContainer>
+  );
 };
+
 export default AddItem;
